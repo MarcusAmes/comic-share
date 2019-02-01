6b74a9b1986013248cb3b266c2b26426e6910e91
Dennis Enwiya <dennisenwiya@gmail.com>
2019-02-01 13:54:34 -0700

    changed

--- front/src/components/Viewer.js
+++ front/src/components/Viewer.js
@@ -6,45 +6,53 @@ class Viewer extends Component{
   
     render(){
         return(
-            <div>
-              <Container >
-                <Row>
-                  <Col sm={{ size: 6, order: 2, offset: 1 }} >
-                <img className="comicImage" src="https://via.placeholder.com/400X600" alt=""/>
+          <div>
+            <Container>
+              <Row>
+                <Col sm={{ size: 6, offset: 1 }}>
+                  <img className="comicImage" src="https://via.placeholder.com/400X600" alt="" />
                 </Col>
-                </Row>
-            <Row>
-              <Col>
-            <Button className="comicButton" color="primary">Back</Button>{' '}
-            </Col>
-            <Col>
-              <Button className="comicButton" color="primary">Next</Button>{' '}
-            </Col>
-           </Row>
+              
+              {/* <Row>
+                <Col>
+                  <Button className="comicButton" color="primary">Back</Button>
+                </Col>
+                <Col>
+                  <Button className="comicButton" color="primary">Next</Button>
+                </Col>
+              </Row> */}
 
-            <Row>
-              <Col xs="6" sm="4">
-            <div className= "commentForm">
-        <form onSubmit={this.addComment}>
-          <div className="field">
-            
-          </div>
-          <div className="field">
-            <div className="control">
-              <textarea className="textarea" name="comment" placeholder="Add a comment"></textarea>
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <button className="button is-primary">Submit</button>
-            </div>
-          </div> 
-        </form>
-            </div>
-            </Col>
-            </Row>
+              
+                <Col sm={{ size: 'auto'}}>
+                  <div className="commentForm">
+                    <form onSubmit={this.addComment}>
+                      <div className="field">
+
+                      </div>
+                      <div className="field">
+                       <div className="control">
+                         <textarea className="textarea" name="comment" placeholder="Add a comment"></textarea>
+                       </div>
+                      </div>
+                      <div className="field">
+                        <div className="control">
+                          <button className="button is-primary">Submit</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Button className="comicButton" color="primary">Back</Button>
+                </Col>
+                <Col>
+                  <Button className="comicButton" color="primary">Next</Button>
+                </Col>
+              </Row>
             </Container>
-            </div>
+          </div>
             
         )
     }
